7f5fe529e15bb8a82cc82e21bb592a13ca464702
Simon Cropp <simon.cropp@gmail.com>
2013-05-07 20:55:41 +1000

    version

--- NServiceBus.Mailer/AssemblyInfo.cs
+++ NServiceBus.Mailer/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NServiceBus.Mailer")]
 [assembly: AssemblyProduct("NServiceBus.Mailer")]
-[assembly: AssemblyVersion("0.2.3.0")]
-[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: AssemblyVersion("0.2.4.0")]
+[assembly: AssemblyFileVersion("0.2.4.0")]
