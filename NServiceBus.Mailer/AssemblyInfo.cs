ae232b45eeea22213975d6b1a1725cd4afa9f95b
Simon Cropp <simon.cropp@gmail.com>
2013-07-11 23:34:09 +1000

    REMOVE PRERELEASE

--- NServiceBus.Mailer/AssemblyInfo.cs
+++ NServiceBus.Mailer/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NServiceBus.Mailer")]
 [assembly: AssemblyProduct("NServiceBus.Mailer")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
